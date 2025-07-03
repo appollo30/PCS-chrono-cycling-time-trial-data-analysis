9a5bcc16761bc7bb5f1fd631f4d817d8034be6a3
appollo30 <leo.lasnier@me.com>
2025-07-03 14:34:02 +0200

    finished tt races.py

--- get_tt_races.py
+++ get_tt_races.py
@@ -1,17 +1,28 @@
-from utils import fetch_async, minutes_to_seconds
+from utils import fetch, fetch_async, minutes_to_seconds, to_numeric
+import pandas as pd
 import aiohttp
 import asyncio
 
-async def parse_race(url, session, verbose=True):
-    soup = await fetch_async(url, session)
+BASE_URL = "https://www.procyclingstats.com/"
+
+async def process_race(url, session, verbose=True):
+    soup = await fetch_async(url,session, verbose=verbose)
     
+    result = parse_race(soup, verbose=verbose)
+    return result
+
+def process_race_sync(url, verbose=True):
+    soup = fetch(url, verbose=verbose)
+    
+    result = parse_race(soup,verbose=verbose)
+    return result
+
+def parse_race(soup, verbose=True):
     result = dict()
     
     result["race_title"] = soup.select_one("head > title").get_text().replace(" results", "")
     if verbose :
-        print(f"Processing race {result["race_title"]}")
-    
-    base_url = "https://www.procyclingstats.com/"
+        print(f"Parsing race {result["race_title"]}")
     
     overall_info = soup.select_one("body > div.wrapper > div.content > div.page-content.noSideNav > div > div.borderbox.w30.right.mb_w100")
     
@@ -21,23 +32,54 @@ async def parse_race(url, session, verbose=True):
     result["departure"] = race_info.select_one("li:nth-child(12) > div.value > a").get_text()
     result["arrival"] = race_info.select_one("li:nth-child(13) > div.value > a").get_text()
     result["class"] = race_info.select_one("li:nth-child(4) > div.value").get_text()
-    result["distance"] = float(race_info.select_one("li:nth-child(6) > div.value").get_text().split()[0])
-    result["vertical_meters"] = int(race_info.select_one("li:nth-child(11) > div.value").get_text())
-    result["startlist_quality"] = int(race_info.select_one("li:nth-child(15) > div.value > a").get_text())
-    result["profile_score"] = int(race_info.select_one("li:nth-child(10) > div.value").get_text())
-    result["temperature"] = float(race_info.select_one("li:nth-child(17) > div.value > a").get_text().split(" ")[0])
-    result["race_ranking"] = int(race_info.select_one("li:nth-child(14) > div.value > a").get_text())
+    result["distance"] = float(race_info.select_one("li:nth-child(6) > div.value").get_text().split()[0]) # in km
     
-    result["winner_time"] = minutes_to_seconds(soup.select_one("#resultsCont > div:nth-child(1) > div > table > tbody > tr:nth-child(1) > td.time.ar").get_text().split(",")[0],sep=".")
+    vertical_meters = race_info.select_one("li:nth-child(11) > div.value").get_text() # in m
+    result["vertical_meters"] = to_numeric(vertical_meters)
     
-    result["profile_image_url"] = base_url + overall_info.select_one("div:nth-child(4) > ul > li > div > a > img").get("src")
+    startlist_quality = race_info.select_one("li:nth-child(15) > div.value > a").get_text()
+    result["startlist_quality"] = to_numeric(startlist_quality)
+    
+    profile_score = race_info.select_one("li:nth-child(10) > div.value").get_text()
+    result["profile_score"] = to_numeric(profile_score)
+    
+    temperature = race_info.select_one("li:nth-child(17) > div.value > a").get_text().split(" ")[0] # in °C
+    result["temperature"] = to_numeric(temperature)
+    
+    race_ranking = race_info.select_one("li:nth-child(14) > div.value > a").get_text()
+    result["race_ranking"] = to_numeric(race_ranking)
+    
+    winner_time_str = soup.select_one("#resultsCont > div:nth-child(1) > div > table > tbody > tr:nth-child(1) > td.time.ar").get_text()
+    if ":" in winner_time_str:
+        result["winner_time"] = minutes_to_seconds(winner_time_str,sep=":") # in seconds
+    else:  
+        result["winner_time"] = minutes_to_seconds(winner_time_str.split(",")[0],sep=".") # in seconds
+        
+    result["winner_speed"] = round(3600*result["distance"]/result["winner_time"],3) # in km/h
+    
+    profile_image_url_extension = overall_info.select_one("div:nth-child(4) > ul > li > div > a > img")
+    
+    if profile_image_url_extension:
+        result["profile_image_url"] = BASE_URL + profile_image_url_extension.get("src")
+    else:
+        result["profile_image_url"] = None
     
     return result
 
+def test_race(url):
+    result = process_race_sync(url, verbose=True)
+    print(result)
+
 if __name__ == "__main__":
     async def main():
-        url = "https://www.procyclingstats.com/race/giro-d-italia/2025/stage-10"
+        results_df = pd.read_csv('data/results.csv')
+        url_set = set(results_df["race_url"].unique())
+        url_list = list(url_set)
         async with aiohttp.ClientSession() as session:
-            result = await parse_race(url, session)
-        print(result)
+            tasks = [process_race(url,session) for url in url_list]
+            data = await asyncio.gather(*tasks)
+        races_df = pd.DataFrame(data)
+        races_df["date"] = pd.to_datetime(races_df["date"])
+        races_df.to_csv("data/races.csv",index=False)
+        
     asyncio.run(main())
