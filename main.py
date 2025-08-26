1c73f9b99894a6674d00a2dbcbf61704e3ecd419
appollo30 <leo.lasnier@me.com>
2025-08-26 20:58:25 +0200

    added awesome progress bar yippee

--- main.py
+++ main.py
@@ -5,24 +5,27 @@ from src.get_tt_results import process_results
 from src.get_tt_races import process_race
 import pandas as pd
 from typing import List, Dict, Set, Tuple
+from tqdm.asyncio import tqdm
 
 async def main():
     # Collecting all time trial specialists
     all_riders = get_all_tt_specialists()
     
     async with aiohttp.ClientSession() as session:
-        # Process all riders concurrently
-        riders_tasks = [process_rider(name, url, session) for name, url in all_riders]
-        riders_data = await asyncio.gather(*riders_tasks)
+        # Process all riders concurrently with progress bar
+        riders_tasks = [process_rider(name, url, session, verbose=False) for name, url in all_riders]
+        print()
+        riders_data = await tqdm.gather(*riders_tasks, desc="Processing riders")
         # Filter out None results
         riders_data = [rider for rider in riders_data if rider]
         
-        # Process all results concurrently
+        # Process all results concurrently with progress bar
         results_tasks = [
-            process_results(rider["url"] + "/results/last-tt-results", session) 
+            process_results(rider["url"] + "/results/last-tt-results", session, verbose=False) 
             for rider in riders_data
         ]
-        results_data = await asyncio.gather(*results_tasks)
+        print()
+        results_data = await tqdm.gather(*results_tasks, desc="Processing results")
         # Flatten the list of lists and filter out None results
         results_data = [result for sublist in results_data if sublist for result in sublist]
         
@@ -31,9 +34,10 @@ async def main():
         for result in results_data:
             race_urls.add(result["race_url"])
         
-        # Process all races concurrently
-        races_tasks = [process_race(url, session) for url in list(race_urls)]
-        races_data = await asyncio.gather(*races_tasks)
+        # Process all races concurrently with progress bar
+        races_tasks = [process_race(url, session, verbose=False) for url in list(race_urls)]
+        print()
+        races_data = await tqdm.gather(*races_tasks, desc="Processing races", )
         # Filter out None results
         races_data = [race for race in races_data if race]
         
@@ -47,6 +51,7 @@ async def main():
     results_df.to_csv("data/results.csv",index=False)
     
     races_df = pd.DataFrame(races_data)
+    races_df["date"] = pd.to_datetime(races_df["date"])
     races_df = races_df.sort_values("date",ascending=False)
     races_df.to_csv("data/races.csv",index=False)
 
